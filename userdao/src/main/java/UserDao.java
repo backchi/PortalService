ef3074cf626c8b14467df12f46b1a0a8b425d00f
backchi <ekgml106@gmail.com>
2018-03-16 17:54:09 +0900

    framework 2

---
+++ userdao/src/main/java/UserDao.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+
+public class UserDao {
+    public User get(int id) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://192.168.0.54/jeju?characterEncoding=utf-8"
+                , "jeju", "jejupw");
+    }
+
+    public Integer insert(User user) throws ClassNotFoundException, SQLException{
+
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo(name, password) values (?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Integer id = resultSet.getInt(1);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return id;
+    }
+}
